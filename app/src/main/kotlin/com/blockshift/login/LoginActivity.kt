b0084147e8fa2938ce84f39e8dea2c964120d2ed
PDempsey03 <123753503+PDempsey03@users.noreply.github.com>
2024-10-01 14:56:56 -0400

    Removed old start screen
    the new start screen is the login fragment

--- app/src/main/kotlin/com/blockshift/login/LoginActivity.kt
+++ app/src/main/kotlin/com/blockshift/login/LoginActivity.kt
@@ -12,7 +12,7 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        loadFragment(StartScreenFragment())
+        loadFragment(LoginFragment())
     }
 
     private fun loadFragment(fragment: Fragment){
