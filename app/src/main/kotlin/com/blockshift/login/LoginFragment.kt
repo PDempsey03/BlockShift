51a0c480dbd115b826da27a0ab4ba7dd668f03b7
jhoyt15 <jacksonhoyt26@gmail.com>
2024-09-30 22:06:12 -0400

    Added login page and demo functionality

--- app/src/main/kotlin/com/blockshift/login/LoginFragment.kt
+++ app/src/main/kotlin/com/blockshift/login/LoginFragment.kt
@@ -2,15 +2,16 @@ package com.blockshift.login
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import com.blockshift.R
+import com.google.android.material.snackbar.Snackbar
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -18,24 +19,70 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class LoginFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
+    /*
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
+    */
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        val view = inflater.inflate(R.layout.fragment_login, container, false)
+
+        val usernameText = view.findViewById<EditText>(R.id.usernameText)
+        val passwordText = view.findViewById<EditText>(R.id.passwordText)
+
+        val enterInfoButton = view.findViewById<Button>(R.id.enterLoginButton)
+        enterInfoButton.setOnClickListener {
+            Log.d("Login Screen","Enter Login Info Button Clicked")
+            val username = usernameText.getText().toString()
+            val password = passwordText.getText().toString()
+            val validLogin = this.verifyLogin(username,password)
+
+            if(validLogin) {
+                Log.d("Login Screen","Valid Login")
+                parentFragmentManager.popBackStack()
+            } else {
+                Log.e("Login Screen", "Invalid Login")
+                val eMessage = Snackbar.make(view,"Invalid Username or Password!", Snackbar.LENGTH_SHORT).setAction("OK") {}
+                eMessage.animationMode = Snackbar.ANIMATION_MODE_SLIDE
+                eMessage.show()
+            }
+        }
+
+        val newUserButton = view.findViewById<Button>(R.id.newUserButton)
+        newUserButton.setOnClickListener {
+            Log.d("Login Screen","New User Button Clicked")
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, CreateAccountFragment()) // fragment_container is the container for fragments
+                .addToBackStack(null)  // This ensures you can go back
+                .commit()
+        }
+        return view
+    }
+
+    private fun verifyLogin(username:String,password:String) : Boolean{
+        //TODO: Use Firebase to verify login
+        Log.d("Login Screen","Username: $username")
+        Log.d("Login Screen","Password: $password")
+
+        val userNamesAndPasswords = mapOf(
+            "Patrick" to "Dempsey1!",
+            "Michael" to "Labib1!",
+            "Jackson" to "Hoyt1!"
+        )
+
+        val validUsername = userNamesAndPasswords.containsKey(username)
+        if(!validUsername) {
+            return false
+        }
+
+
+        return (userNamesAndPasswords[username].equals(password))
     }
 
     companion object {
@@ -43,17 +90,13 @@ class LoginFragment : Fragment() {
          * Use this factory method to create a new instance of
          * this fragment using the provided parameters.
          *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
          * @return A new instance of fragment LoginFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance() =
             LoginFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
                 }
             }
     }
