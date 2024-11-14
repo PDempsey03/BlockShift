b820de3ca27268e06c4969c40aa9ad236f3394e5
PDempsey03 <123753503+PDempsey03@users.noreply.github.com>
2024-11-14 15:35:50 -0500

    Added better time formatting
    Now displays time in minutes, seconds, and milliseconds. Anything longer than or equal to an hour will instead display a message about it being too long.

--- android/src/main/kotlin/com/blockshift/model/HighScoreAdapter.kt
+++ android/src/main/kotlin/com/blockshift/model/HighScoreAdapter.kt
@@ -11,6 +11,13 @@ import com.blockshift.R
 
 class HighScoreAdapter(private val highScores: List<HighScoreDisplayData>, private val highScoreType: String, private val startingRank: Int) : RecyclerView.Adapter<HighScoreAdapter.HighScoreViewHolder>() {
 
+    private val milliSecondsInSecond = 1000L
+    private val secondsInMinute = 60
+    private val minutesInHour = 60
+    private val maxDisplayTime = (((minutesInHour - 1) * secondsInMinute * milliSecondsInSecond) + ((secondsInMinute - 1) *
+        milliSecondsInSecond) +
+        (milliSecondsInSecond - 1)) // max mins + max secs + max ms
+
     class HighScoreViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val rankTextView: TextView = itemView.findViewById(R.id.high_score_row_item_rank)
         val displayNameTextView: TextView = itemView.findViewById(R.id.high_score_row_item_display_name)
@@ -34,7 +41,21 @@ class HighScoreAdapter(private val highScores: List<HighScoreDisplayData>, priva
         holder.displayNameTextView.text = highScoreItem.userData.displayname
         holder.usernameTextView.text = "@" + highScoreItem.userData.username
         holder.highScoreTypeTextView.text = when(highScoreType) {
-            HighScoreTableNames.TIME -> highScoreItem.highScoreData.time.toString()
+            HighScoreTableNames.TIME -> {
+                // need to properly format time
+                val totalMilliseconds = highScoreItem.highScoreData.time
+
+                // wont display anything longer than or equal to 1 hour
+                if(totalMilliseconds > maxDisplayTime) {
+                    holder.itemView.context.getString(R.string.high_score_beyond_max_time).uppercase()
+                } else {
+                    val totalSeconds = totalMilliseconds / milliSecondsInSecond
+                    val remainingMilliseconds = totalMilliseconds % milliSecondsInSecond
+                    val totalMinutes = totalSeconds / secondsInMinute
+                    val remainingSeconds = totalSeconds % secondsInMinute
+                    String.format("%02d:%02d:%03d", totalMinutes, remainingSeconds, remainingMilliseconds)
+                }
+            }
             HighScoreTableNames.DISTANCE -> highScoreItem.highScoreData.distance.toString()
             HighScoreTableNames.MOVES -> highScoreItem.highScoreData.moves.toString()
             else -> "N/A"
