bea74f1fb56f786b1707ab7d8ab93a06fa716cdf
PDempsey03 <123753503+PDempsey03@users.noreply.github.com>
2024-11-21 15:17:24 -0500

    Added username tests
    now tests for invalid and valid usernames

---
+++ android/src/androidTest/kotlin/UsernameTests.kt
@@ -0,0 +1,58 @@
+import com.blockshift.ui.login.LoginManager
+import org.junit.Test
+
+class UsernameTests {
+    @Test
+    fun validUsernames() {
+        val standardUsername = "username"
+        val startUppercase = "Username"
+        val endUppercase = "usernamE"
+        val containsUppercase = "useRname"
+        val startsDigit = "1username"
+        val endsDigit = "username9"
+        val containsDigit = "user5name"
+
+        val usernames = listOf(
+            standardUsername,
+            startUppercase,
+            endUppercase,
+            containsUppercase,
+            startsDigit,
+            endsDigit,
+            containsDigit
+        )
+
+        // ensure all valid usernames are returning as valid
+        for(username in usernames) {
+            assert(LoginManager.isValidUsername(username)) {
+                "username $username was found to be invalid"
+            }
+        }
+    }
+
+    @Test
+    fun invalidUsernames() {
+        val justShortLength = "pat"
+        val veryShortLength = "p"
+        val justLongLength = "thisIsJustTooLong"
+        val veryLongLength = "ThisIsWayToLongToBeAUsernameButWhyNotTry"
+        val containsNonAlphanumericSpace = "This hasASpace"
+        val containsNonAlphanumericSymbol = "ThisIsAn@"
+
+        val usernames = listOf(
+            justShortLength,
+            veryShortLength,
+            justLongLength,
+            veryLongLength,
+            containsNonAlphanumericSpace,
+            containsNonAlphanumericSymbol
+        )
+
+        // ensure all invalid usernames are returning as invalid
+        for(username in usernames) {
+            assert(!LoginManager.isValidUsername(username)){
+                "username $username was found to be valid"
+            }
+        }
+    }
+}
